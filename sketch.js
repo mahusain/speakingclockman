c7be00681f0f2bc6fd522fffc7fa15b3f40ac841
mahusain <48697881+mahusain@users.noreply.github.com>
2021-03-12 22:18:08 -0800

    Audio fix

--- sketch.js
+++ sketch.js
@@ -7,43 +7,54 @@ let meSpeak2;
 let micLevel;
 let hatBeat = 10;
 let moveVar = 0.77;
+let sketchStarted = false;
 
 function setup(){
   createCanvas(400, 400);
   angleMode(DEGREES);
-  
+
+  createButton("START").mousePressed(startSketch);
+
+
+
+
+
+}
+
+function startSketch(){
+  console.log("StartSketch!")
   mic = new p5.AudioIn();
   mic.start();
   micLevel = mic.getLevel();
- 
-   
-  
-  
-}
 
+  sketchStarted = true;
+
+}
 function draw(){
-  
-  background(200, 200, 100);
-  
-  console.log("mouse y is " + mouseX);
-  console.log("mouse y is " + mouseY);
-  console.log("mic level is " + mic.getLevel());
-  meSpin = map(mouseX)
-  meSpeak = map(mic.getLevel(), 0, 1, 0.1, 100)
-  meSpeak2 = map(mic.getLevel(), 0, 1, 0.1, -100)
-  
-  
- 
-  
-  drawBody();
-  drawHead();
-  drawHat(hatBeat);
-  drawFace();
-  drawTopMouth();
-  drawBottomMouth();
-  drawNose(meSpin);
-  movement1();
-  
+  if(sketchStarted){
+    background(200, 200, 100);
+
+    console.log("mouse y is " + mouseX);
+    console.log("mouse y is " + mouseY);
+    console.log("mic level is " + mic.getLevel());
+    meSpin = map(mouseX)
+    meSpeak = map(mic.getLevel(), 0, 1, 0.1, 100)
+    meSpeak2 = map(mic.getLevel(), 0, 1, 0.1, -100)
+
+
+
+
+    drawBody();
+    drawHead();
+    drawHat(hatBeat);
+    drawFace();
+    drawTopMouth();
+    drawBottomMouth();
+    drawNose(meSpin);
+    movement1();
+  }
+
+
 }
 function drawHat(){
   fill(10);
@@ -55,32 +66,32 @@ function drawHat(){
   stroke(0);
   strokeWeight(10);
   rect(70, hatBeat + 60, 250, 50);
-  
+
 
 }
 
 function mousePressed(){
-  
+
    if(mouseX > 200 && mouseX < 400 && mouseY > 100 && mouseY < 300) {
       console.log("mouse beep");
       }
 }
 
-  
+
 
 function drawBody(){
-  
+
   fill(200);
   stroke(0);
   strokeWeight(10);
   rect(170, 290, 50, 50);
-  
+
   fill(10);
   stroke(0);
   strokeWeight(10);
   rect(120, 350, 150, 150);
-  
-  
+
+
 }
 
 function drawHead(){
@@ -93,7 +104,7 @@ function drawHead(){
 function drawFace(){
   let r1 = map(mouseX, 0, width, 0, height);
   let r2 = height - r1;
-  
+
   fill(50);
   ellipse(160, 170, 10, 50);
   fill(50);
@@ -118,14 +129,14 @@ function drawBottomMouth(){
 }
 
 function drawNose(xPos, yPos, rotation){
-  
+
   line(200, 200, mouseX, 55);
-  
+
 }
 function movement1() {
   hatBeat = hatBeat + moveVar;
   if ((frameCount % 10) == 0) {
     moveVar = moveVar*-1;
   }
-  
+
 }
